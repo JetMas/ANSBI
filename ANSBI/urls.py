a1b3f6939a9e1d4b0a1addba4423f5c8e93d9183
JetMas <jethromasangya@gmail.com>
2017-08-01 09:17:11 -0600

    Changed the urls file to only include the admin and employment

--- ANSBI/urls.py
+++ ANSBI/urls.py
@@ -20,7 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('Base.urls')),
-    url(r'^employment/', include('Employment.urls')),
+    url(r'', include('Employment.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
